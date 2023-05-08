bc8b88614ee89b71b434642661c7ab6f3ea1fe96
kev87ian <kelvinian87@gmail.com>
2023-05-08 22:30:29 +0300

    Wrote the apps navgraph

---
+++ app/src/main/java/com/kev/windowshopper/navigation/BottomNavGraph.kt
@@ -0,0 +1,31 @@
+package com.kev.windowshopper.navigation
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+@Composable
+fun BottomNavGraph(
+    navHostController: NavHostController
+) {
+    NavHost(navController = navHostController, startDestination = BottomBarScreens.Jumia.route) {
+        composable(route = BottomBarScreens.Jumia.route) {
+            Screen(text = "Jumia")
+        }
+
+        composable(route = BottomBarScreens.Amazon.route) {
+            Screen(text = "Amazon")
+        }
+
+        composable(route = BottomBarScreens.WalMart.route) {
+            Screen(text = "Walmart")
+        }
+    }
+}
+
+@Composable
+fun Screen(text: String) {
+    Text(text = text)
+}
