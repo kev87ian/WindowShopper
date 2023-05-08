194fc364a64c6dc0810fddb1746eb75376cafa48
kev87ian <kelvinian87@gmail.com>
2023-05-08 23:07:21 +0300

    completed navigation setup

--- app/src/main/java/com/kev/windowshopper/MainActivity.kt
+++ app/src/main/java/com/kev/windowshopper/MainActivity.kt
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.kev.windowshopper.ui.MainScreen
 import com.kev.windowshopper.ui.theme.WindowShopperTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,10 +20,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    MainScreen()
                 }
             }
         }
     }
 }
-
---
+++ app/src/main/java/com/kev/windowshopper/ui/MainScreen.kt
@@ -0,0 +1,111 @@
+package com.kev.windowshopper.ui
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.kev.windowshopper.navigation.BottomBarScreens
+import com.kev.windowshopper.navigation.BottomNavGraph
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+    var query by remember {
+        mutableStateOf("")
+    }
+    Scaffold(
+        bottomBar = {
+            BottomBar(navController = navController)
+        }
+    ) {
+        Column (
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth().padding(12.dp),
+                value = query,
+                onValueChange = {
+                    query = it
+                },
+                placeholder = {
+                    Text(text = "Search...")
+                },
+                leadingIcon = {
+                    Icons.Default.Search
+                }
+            )
+        }
+        BottomNavGraph(navHostController = navController)
+    }
+}
+
+@Composable
+fun BottomBar(navController: NavHostController) {
+    val screens = listOf(
+        BottomBarScreens.Jumia,
+        BottomBarScreens.Amazon,
+        BottomBarScreens.WalMart
+    )
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+    NavigationBar {
+        screens.forEach { screen ->
+            AddItem(
+                screen = screen,
+                currentDestination = currentDestination,
+                navController = navController
+            )
+        }
+    }
+}
+
+@Composable
+fun RowScope.AddItem(
+    screen: BottomBarScreens,
+    currentDestination: NavDestination?,
+    navController: NavHostController
+) {
+    NavigationBarItem(
+        selected = currentDestination?.hierarchy?.any {
+            it.route == screen.route
+        } == true,
+        label = {
+            Text(text = screen.title)
+        },
+        icon = {
+            Icon(imageVector = screen.icon, contentDescription = "Navigation icon")
+        },
+        onClick = {
+            navController.navigate(screen.route)
+        },
+        alwaysShowLabel = true,
+        enabled = true
+
+    )
+}
