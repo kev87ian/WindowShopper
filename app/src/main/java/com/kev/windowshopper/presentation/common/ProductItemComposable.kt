08d7c8da3a2feb77b64f84f0162c9303a68a36fd
kev87ian <kelvinian87@gmail.com>
2023-06-09 14:32:59 +0300

    still improving the UI

--- app/src/main/java/com/kev/windowshopper/presentation/common/ProductItemComposable.kt
+++ app/src/main/java/com/kev/windowshopper/presentation/common/ProductItemComposable.kt
@@ -1,25 +1,39 @@
 package com.kev.windowshopper.presentation.common
 
+import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
+import com.kev.windowshopper.R
 import com.kev.windowshopper.domain.model.Product
 
 @Composable
@@ -27,37 +41,67 @@ fun ProductItemComposable(
     product: Product
 ) {
     val context = LocalContext.current
-    Card(modifier = Modifier.background(MaterialTheme.colorScheme.background).padding(12.dp)) {
-        Box(modifier = Modifier.fillMaxSize()) {
-            AsyncImage(
-                model = ImageRequest.Builder(context)
-                    .data(product.productImageLink)
-                    .crossfade(true).build(),
-                contentScale = ContentScale.FillBounds,
-                contentDescription = "",
-                modifier = Modifier.fillMaxWidth()
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(
-                        Brush.verticalGradient(
-                            colors = listOf(
-                                Color.Transparent,
-                                Color.Black
-                            ),
-                            startY = 300f
-                        )
+
+    Card(
+        shape = RoundedCornerShape(16.dp),
+        modifier = Modifier
+            .height(300.dp)
+            .background(MaterialTheme.colorScheme.background)
+    ) {
+        Column {
+            Box(modifier = Modifier.fillMaxWidth()) {
+                AsyncImage(
+                    model = ImageRequest.Builder(context)
+                        .crossfade(true)
+                        .data(product.productImageLink)
+                        .build(),
+                    contentDescription = "Product's Image"
+                )
+
+                IconButton(onClick = {
+                    Toast.makeText(context, "Clicked favorite", Toast.LENGTH_SHORT).show()
+                }, modifier = Modifier.align(Alignment.TopEnd)) {
+                    Icon(
+                        imageVector = Icons.Default.FavoriteBorder,
+                        contentDescription = "Favorite Button"
                     )
+                }
+            }
+
+            Text(
+                text = product.productName,
+                fontFamily = FontFamily.SansSerif,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 14.sp,
+                textAlign = TextAlign.Center,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
             )
 
-            Box(modifier = Modifier.align(Alignment.BottomCenter)) {
-                Text(text = product.productName, style = TextStyle(fontSize = 14.sp))
-            }
+            Spacer(modifier = Modifier.height(8.dp))
 
-            Box(modifier = Modifier.align(Alignment.BottomEnd)) {
-                Text(text = "2 reveies")
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.star),
+                        contentDescription = "",
+                        modifier = Modifier.size(12.dp)
+                    )
+                    Text(text = " | ".plus(product.totalReviews))
+                }
+
+                Text(
+                    text = product.productPrice,
+                    fontWeight = FontWeight.Black,
+                    modifier = Modifier.padding(end = 8.dp)
+                )
             }
-        } //
+        }
     }
 }
